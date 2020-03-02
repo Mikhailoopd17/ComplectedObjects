a49e9753910319fab45c6448b093670cc09da5df
Михаил <maike777@yandex.ru>
2020-03-02 14:29:23 +0500

    Добавлена функция добавления объектов

---
+++ src/main/java/group/object_registry/Entity/SomeObj.java
@@ -0,0 +1,22 @@
+package group.object_registry.Entity;
+
+public class SomeObj {
+    private EntityObject entityObject;
+    private String custom;
+
+    public EntityObject getEntityObject() {
+        return entityObject;
+    }
+
+    public void setEntityObject(EntityObject entityObject) {
+        this.entityObject = entityObject;
+    }
+
+    public String getCustom() {
+        return custom;
+    }
+
+    public void setCustom(String custom) {
+        this.custom = custom;
+    }
+}
